d7da332f7fac9742e90082e77b1c5f89f667926b
Frank Bergkemper <frank.bergkemper@bareos.com>
2019-02-14 17:57:16 +0100

    webui: cleanup

--- web/webui/src/views/Console.vue
+++ web/webui/src/views/Console.vue
@@ -1,9 +1,9 @@
 <template>
-	<section>
-	    <div id="log" style="margin-top:20px;">
-			    <my-terminal :terminal="terminal" ref="xterm"></my-terminal>
-	    </div>
-	</section>
+  <section>
+    <div id="log" style="margin-top:20px;">
+      <my-terminal :terminal="terminal" ref="xterm"></my-terminal>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -20,18 +20,18 @@ export default {
       logtxt: '',
       input: 0,
       command: '',
-			 terminal: {
-			  pid: 1,
-			  name: 'terminal',
-			  cols: 1000,
+      terminal: {
+        pid: 1,
+        name: 'terminal',
+        cols: 1000,
         rows: 1000,
         cursorBlink: true
       }
-      
+
     }
   },
   methods: {
-    
+
   },
   mounted () {
     this.$refs.xterm.term.write('* ')
@@ -39,7 +39,7 @@ export default {
     this.$refs.xterm.term.on('data', (data) => {
       const code = data.charCodeAt(0)
 
-      if (code == 13) {
+      if (code === 13) {
         // CR received
         console.log(this.command)
         this.$socket.send(this.command)
@@ -48,7 +48,7 @@ export default {
           element = element.replace(/\n/g, '\r\n')
           this.$refs.xterm.term.write(element)
         })
-        
+
         this.$refs.xterm.term.writeln('')
         this.$refs.xterm.term.fit()
 
@@ -57,7 +57,7 @@ export default {
         // this.$refs.xterm.term.write("\r\nYou typed: '" + this.command + "'\r\n")
         this.$refs.xterm.term.write('* ')
         this.command = ''
-      } else if (code < 32 || code == 127) {
+      } else if (code < 32 || code === 127) {
         // Control codes
 
       } else {
@@ -65,9 +65,7 @@ export default {
         this.$refs.xterm.term.write(data)
         this.command += data
       }
-
     })
-    
   },
   created () {
     this.$store.state.socket.message = []
@@ -76,7 +74,7 @@ export default {
     this.$store.state.socket.message = []
   },
   components: {
-		    'my-terminal': Console
+    'my-terminal': Console
   },
   computed: {
 
